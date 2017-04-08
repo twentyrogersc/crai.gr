35e8feac397eeb940de2d3f76a634782ae8195e8
Craig Rogers <20rogersc@gmail.com>
2017-04-08 09:18:33 +0100

    Moved from harp to next.

---
+++ components/Head.js
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default () => (
+  <Head>
+    <title>Craig Rogers</title>
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+    />
+    <link rel="stylesheet" href="/static/styles.css" />
+  </Head>
+);
--- server.js
+++
@@ -1,4 +0,0 @@
-
-const harp = require('harp');
-
-harp.server('src', { port: process.env.PORT || 5000 }, () => {});
