a448e23aa34873620c6357cfcdc7a10b49f2d75d
Craig Rogers <20rogersc@gmail.com>
2017-09-05 12:01:58 +0100

    Updated copy.

--- pages/cv.js
+++ pages/cv.js
@@ -15,78 +15,44 @@ export default () => (
         {' '}
         |
         {' '}
-        <a href="https://uk.linkedin.com/in/craigarogers">
-          linkedin.com/in/craigarogers
-        </a>
+        <a href="https://uk.linkedin.com/in/craigarogers">linkedin.com/in/craigarogers</a>
       </strong>
     </p>
     <p>
-      An experienced front-end developer just wanting to make great things. With interests spanning
-      the latest in both design and technology, successful projects are developed at both a technical and
-      conceptual level.
+      An experienced and award-winning front-end developer just wanting to make great things. With interests spanning the latest in both design and technology, successful projects are developed at both a technical and conceptual level.
     </p>
-    <h2 id="skills">Skills</h2>
+
+    <h2>Skills</h2>
     <ul>
-      <li>HTML5, Jade, Templating (Django, Mustache, etc)</li>
-      <li>CSS3, SASS, Stylus</li>
-      <li>JavaScript, Browserify, RequireJS, jQuery, Backbone.js, Canvas</li>
-      <li>Grunt, Gulp.js</li>
-      <li>Node.js, MongoDB, PHP, MySQL</li>
-      <li>UX, Wireframing, Photoshop, Illustrator</li>
+      <li>
+        <strong>JavaScript</strong> ES6, React, Redux, Backbone, Canvas, Webpack, Gulp, Browserify
+      </li>
+      <li><strong>HTML5</strong> Templating (Django, Mustache, etc), Jade</li>
+      <li><strong>CSS3</strong> Modules, PostCSS, BEM, Sass, Stylus</li>
+      <li><strong>Testing/Deployment</strong> Mocha, ESLint, NPM, CircleCI, Node.js, TDD</li>
+      <li><strong>UI/UX</strong> Wireframing, Sketch, Photoshop, Research</li>
     </ul>
-    <h2 id="projects">Projects</h2>
-    <h3 id="-happy-party-https-www-globalhappyparty-com-">
-      <a href="https://www.globalhappyparty.com">Happy Party</a>
-    </h3>
-    <p><strong>Google/The Rumpus Room, 2015</strong></p>
-    <p>
-      A responsive infinite-scroll animated gallery displaying user generated content, submitted via a
-      JavaScript webcam photobooth tool.
-    </p>
-    <blockquote>
-      <p>Gulp, HTML, Stylus, Browserify, JavaScript (Canvas, GetUserMedia)</p>
-    </blockquote>
-    <h3 id="-denim-studio-http-trr-tv-selfridges-">
-      <a href="http://trr.tv/selfridges">
-        Denim
-        Studio
-      </a>
-    </h3>
-    <p><strong>Selfridges/The Rumpus Room, 2013</strong></p>
-    <p>
-      A suite of scripts that dynamically created and powered a navigable mosaic website, with the
-      ability to control/moderate/tweak the composition at each stage.
-    </p>
-    <blockquote>
-      <p>Node.js, Photoshop JSX, SVGs, HTML, SASS, JavaScript (Canvas)</p>
-    </blockquote>
-    <h2 id="experience">Experience</h2>
-    <h3 id="front-end-developer">Front-end developer</h3>
+
+    <h2>Experience</h2>
+    <h3>Lead front-end developer</h3>
     <p>
       <strong>
-        <a href="http://knyttan.com">Knyttan</a>, May 2015 - Present
-      </strong>
-    </p>
-    <h3 id="front-end-developer">Front-end developer</h3>
-    <p>
-      <strong>
-        <a href="http://www.trr.tv">The Rumpus Room</a>, Sep 2010 – Apr 2015
+        <a href="https://unmade.com">Unmade</a>, May 2015 - Aug 2017
       </strong>
     </p>
     <p>
-      Lead front-end development within the studio, involving feasibility analysis, prototyping,
-      development and testing. Projects were mainly large JavaScript web builds centred around the latest
-      HTML5 technologies (video, canvas, getUserMedia, etc), great user experiences and responsive design
-      implementation. Clients included Google, Selfridges, Eurovision, Tate, P&G and the
-      International Olympic Committee.
+      Overseeing all front-end work within the company, with a strong focus on building a scalable and flexible product. Introduced build tools, styleguides (components), testing and continuous deployment into the stack. Projects mainly surround the core Unmade products, consisting of data-driven graphical programming (canvas) and interface implementation (react, redux, apis, etc).
     </p>
-    <blockquote>
-      <p>
-        HTML/Jade, JavaScript/jQuery/Backbone.js, CSS/Stylus/SASS, RequireJS/Browserify,
-        Gulp/Grunt
-      </p>
-    </blockquote>
-    <h3 id="web-developer">Web developer</h3>
+    <h3>Front-end developer</h3>
+    <p>
+      <strong>
+        <a href="https://vimeo.com/therumpusroom">The Rumpus Room</a>, Sep 2010 – Apr 2015
+      </strong>
+    </p>
+    <p>
+      Lead front-end development within the studio, involving feasibility analysis, prototyping, development and testing. Projects were mainly large JavaScript web builds centred around the latest HTML5 technologies (video, canvas, getUserMedia, etc), great user experiences and responsive design implementation. Clients included Google, Selfridges, Eurovision, Tate, P&G and the International Olympic Committee.
+    </p>
+    <h3>Web developer</h3>
     <p>
       <strong>
         <a href="http://www.fourcommunications.com/">Four Communications</a>
@@ -95,18 +61,33 @@ export default () => (
       </strong>
     </p>
     <p>
-      Full-stack development, working on global websites with a focus on accessibility and
-      multi-lingual functionality. Clients included Sony Computer Entertainment Europe, HSBC and the
-      Office for National Statistics (Census 2011).
+      Full-stack development, working on global websites with a focus on accessibility and multi-lingual functionality. Clients included Sony Computer Entertainment Europe, HSBC and the Office for National Statistics (Census 2011).
     </p>
-    <blockquote>
-      <p>PHP/MySQL, HTML, JavaScript/jQuery, CSS/SASS</p>
-    </blockquote>
-    <h2 id="education">Education</h2>
-    <h3 id="goldsmiths-college-university-of-london">
-      Goldsmiths College, University of London
-    </h3>
-    <p><strong>2008 – 2011</strong></p>
-    <p>Bachelor of Arts (BA), Computing & Interaction Design</p>
+
+    <h2>Projects</h2>
+    <h3><a href="https://umd.studio">UMd</a></h3>
+    <p><strong>Unmade, 2016</strong></p>
+    <p>
+      Part of the D&AD Yellow Pencil winning project, an ecommerce showcase offering interactive and customisable products, powered by highly configurable JavaScript modules.
+    </p>
+    <h3><a href="https://www.globalhappyparty.com">Happy Party</a></h3>
+    <p><strong>Google/The Rumpus Room, 2015</strong></p>
+    <p>
+      As featured on the Google homepage, a responsive infinite-scroll animated gallery displaying user generated content, submitted via a JavaScript webcam photobooth tool.
+    </p>
+    <h3><a href="https://vimeo.com/71008158">Denim Studio</a></h3>
+    <p><strong>Selfridges/The Rumpus Room, 2013</strong></p>
+    <p>
+      A suite of scripts that dynamically created and powered a navigable mosaic website, with the ability to control/moderate/tweak the composition at each stage.
+    </p>
+
+    <h2>Education</h2>
+    <h3>Bachelor of Arts (BA), Computing & Interaction Design</h3>
+    <p>
+      <strong>
+        <a href="http://www.gold.ac.uk/">Goldsmiths College, University of London</a>, 2008 – 2011
+      </strong>
+    </p>
+    <p />
   </div>
 );
